93d397b7957377c14c1770ca7d3e4be8f9d7ef46
munyokii <munyoki912@gmail.com>
2026-01-30 23:24:38 +0300

    Connecting to mongo db using mongoose

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 
 import { LinksModule } from './links/links.module';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 
 @Module({
-  imports: [LinksModule, ConfigModule.forRoot()],
+  imports: [LinksModule, ConfigModule.forRoot(), MongooseModule.forRoot('mongodb://localhost/nest')],
   controllers: [AppController],
   providers: [AppService],
 })
